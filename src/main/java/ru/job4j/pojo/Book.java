c384757f81b3b9bdfb171816149271aaacf0bfec
Bezon <bezonbezon@yandex.ru>
2022-12-14 21:20:18 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private int count;
+    private String name;
+
+    public Book(int count, String name) {
+        this.count = count;
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book = new Book(12, "Jungle");
+        Book book2 = new Book(102, "Happy");
+        Book book3 = new Book(75, "Year");
+        Book book4 = new Book(100, "Clean code");
+        Book[] books = {book, book2, book3, book4};
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " " + books[i].getCount());
+        }
+        Book bookTemp = books[3];
+        books[3] = books[0];
+        books[0] = bookTemp;
+        System.out.println("Change books!");
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName() + " " + books[i].getCount());
+        }
+        System.out.println("Books with name \"Clean code\"");
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getName() == "Clean code") {
+                System.out.println(books[i].getName() + " " + books[i].getCount());
+            }
+        }
+    }
+}
