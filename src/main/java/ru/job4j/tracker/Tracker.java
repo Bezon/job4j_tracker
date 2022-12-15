8cbc65c696cf56d80eda5beb0ba40d6b44da25f3
Bezon <bezonbezon@yandex.ru>
2022-12-15 21:20:48 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -19,6 +21,39 @@ public class Tracker {
                 rsl = item;
                 break;
             }
+        }
+        return rsl;
+    }
+    public Item[] findAll(){
+        Item[] rsl = new Item[this.size];
+        int size = 0;
+        for (int index = 0; index < this.size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+        return rsl;
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl = new Item[this.size];
+        int size = 0;
+        for (int index = 0; index < this.size; index++) {
+            Item item = items[index];
+            if (name.equals(item.getName())) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
         }
         return rsl;
     }
