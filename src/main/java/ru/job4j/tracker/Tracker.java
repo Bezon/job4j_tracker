b9282148b8426cf95a1f30c844d47a2d9a033732
Bezon <bezonbezon@yandex.ru>
2022-12-16 13:02:09 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #371587]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,13 +59,13 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
+        boolean resl = index != -1;
+        if (resl) {
             items[index] = null;
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
-        return rsl;
+        return resl;
     }
 }
