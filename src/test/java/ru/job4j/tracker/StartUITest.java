db30608a76e00fa9bb1f689ce3f0b1159776dc0b
Bezon <bezonbezon@yandex.ru>
2022-12-27 14:27:03 +0300

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -40,7 +40,6 @@ public class StartUITest {
         actions.add(new CreateAction(out));
         actions.add(new Exit(out));
         new StartUI(out).init(in, tracker, actions);
-        //assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
         assertThat(tracker.findAll().get(0).getName()).isEqualTo("Item name");
     }
 
