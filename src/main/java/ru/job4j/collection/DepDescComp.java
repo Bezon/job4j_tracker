cf001fa8baab7e3bea2a4537dd87bb300397da23
Bezon <bezonbezon@yandex.ru>
2023-01-08 14:07:32 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,16 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] path1 = o1.split("/");
         String[] path2 = o2.split("/");
         int rsl = path2[0].compareTo(path1[0]);
-        for (int i = 1; i < Math.min(path1.length, path2.length); i++) {
-            if (rsl == 0) {
-                rsl = path1[i].compareTo(path2[i]);
-            } else {
-                break;
-            }
-        }
-        if (rsl == 0) {
-            rsl = Integer.compare(path1.length, path2.length);
-        }
-        return rsl;
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
